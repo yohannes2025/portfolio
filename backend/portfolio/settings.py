6a83b46a3b7f26ab66988d26a5f3192760fa9146
yohannes2025 <johannes.m.tekle@gmail.com>
2026-05-18 23:58:58 +0200

    Create backend django setting

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-
     'api',
 ]
 
